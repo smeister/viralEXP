ed8803c44207567b5ae2f57f284bb13185be48ed
smeister <s.meister.romero@gmail.com>
2017-11-21 18:49:36 +0100

    update test file

--- R/test.R
+++ R/test.R
@@ -1,12 +1,9 @@
-DFtest<-read.csv("D:/Dropbox/EPFL/Lab book/R/template.csv",sep=";", header = T)
+#DFtest<-read.csv("D:/Dropbox/EPFL/Lab book/R/template.csv",sep=";", header = T)
 
-getALL(DFtest)
+#getALL(DFtest)
 
-test1<-getMPN(x=c(5,5,5,5,5,1,0,0), n=5, v=0.1)
-test2<-getMPN(x=c(6,6,3,0,0,0,0), n=6, v=0.1)
+#test1<-getMPN(x=c(5,5,5,5,5,1,0,0), n=5, v=0.1)
+#test2<-getMPN(x=c(6,6,3,0,0,0,0), n=6, v=0.1)
 
-plotMPN(test1,test2)
+#plotMPN(test1,test2)
 
-remove.packages("viralEXP")
-library(devtools)
-install_github("smeister/viralEXP")
