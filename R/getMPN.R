8168b6dfcc450e160dff87d34a3e410c0c6ae2ac
smeister <s.meister.romero@gmail.com>
2018-03-22 17:44:11 +0100

    Change the output of getMPN function if x is "NA" or equals to 0.

--- R/getMPN.R
+++ R/getMPN.R
@@ -16,16 +16,20 @@ getMPN=function(x,n,v,d=10){
   if (length(n)==1) {
     n<-rep(n,length(x))
   }
-  if(is.na(x[1]) | sum(x)==0){
+  if(is.na(x[1])){
     return(list("raw.data"=data.frame("x"=rep(NA,length(x)), "n"=n, "v"=v),"Results"=data.frame('MPNCU.ml'=NA, "Upper 95CI"=NA, "Lower 95CI"=NA, "Std.err"=NA)))
   } else {
-    MPN=as.vector(mle2(function(mu){sum(suppressWarnings((n-x)*mu*v-x*log(1-exp(-mu*v))))},start=list(mu=1),method="SANN",optimizer="nlminb")@details$par)
-    SE=(MPN^2*sum((n*v^2)/(exp(MPN*v)-1)))^-0.5 # Standard error
-    LL=signif(exp(log(MPN)-qnorm(1-0.05/2,0,1)*SE),digits=3) # Lower 95% confidence interval
-    UL=signif(exp(log(MPN)+qnorm(1-0.05/2,0,1)*SE),digits=3) # Upper 95% confidence interval
-    output<-list("raw.data"=data.frame("x"=x, "n"=n, "v"=v),"Results"=data.frame('MPNCU.ml'=MPN, "Upper 95CI"=UL, "Lower 95CI"=LL, "Std.err"=SE))
-    class(output)<-"MPN"
-    return(output)
+    if (sum(x)==0) {
+      return(list("raw.data"=data.frame("x"=rep(0,length(x)), "n"=n, "v"=v),"Results"=data.frame('MPNCU.ml'=0, "Upper 95CI"=0, "Lower 95CI"=0, "Std.err"=0)))
+    } else {
+      MPN=as.vector(mle2(function(mu){sum(suppressWarnings((n-x)*mu*v-x*log(1-exp(-mu*v))))},start=list(mu=1),method="SANN",optimizer="nlminb")@details$par)
+      SE=(MPN^2*sum((n*v^2)/(exp(MPN*v)-1)))^-0.5 # Standard error
+      LL=signif(exp(log(MPN)-qnorm(1-0.05/2,0,1)*SE),digits=3) # Lower 95% confidence interval
+      UL=signif(exp(log(MPN)+qnorm(1-0.05/2,0,1)*SE),digits=3) # Upper 95% confidence interval
+      output<-list("raw.data"=data.frame("x"=x, "n"=n, "v"=v),"Results"=data.frame('MPNCU.ml'=MPN, "Upper 95CI"=UL, "Lower 95CI"=LL, "Std.err"=SE))
+      class(output)<-"MPN"
+      return(output)
+    }
   }
 }
 
